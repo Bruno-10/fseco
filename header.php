f2691c673e46e5e34a26666be110e17dc8dbf846
santibriozzo <santibriozzo@gmail.com>
2019-09-20 15:56:53 -0300

    Header, header.css, imagen

---
+++ header.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://unpkg.com/ionicons@4.5.10-0/dist/ionicons.js"></script>
+    <link rel="stylesheet" href="css/master.css">
+    <link rel="stylesheet" href="css/index.css">
+    <title>Home</title>
+</head>
+    <body>
+        <div class="container-fluid padding">
+                    <header class="row col-sm-12 col-md-12 col-lg-12 margencero">
+                            <div class="col-sm-12 col-md-12 col-lg-12 navbar navbar-expand-md bg-dark navbar-dark">
+                                <div class="logo col-sm-12 col-md-6 col-lg-3 justify-content-center">
+                                    <a class="navbar-brand" href="#"><img class="col-sm-12 col-md-6 col-lg-12" src="img/logoppal.jpg" alt="logoprincipal"></a>
+                                </div>
+                                <nav class="col-sm-12 col-md-6 col-lg-6 navbar navbar-light justify-content-center ">
+                                        <form class="form-inline flex-wrap ">
+                                            <input class="caja_busc" type="search" placeholder="¿Qué estas buscando?" aria-label="Search">
+                                            <button type="submit"><ion-icon name="search" class="icono"></ion-icon></button>
+                                        </form>
+                                </nav>
+                            </div>
+                            <nav class=" col-sm-12 col-md-12 col-lg-12 navbar navbar-expand-md bg-dark navbar-dark">   
+                                            <div class="hamburguesa align-items-right">
+                                                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+                                                <span class="navbar-toggler-icon"></span>
+                                                </button>   
+                                            </div>
+                                            <div class="collapse navbar-collapse" id="collapsibleNavbar">
+                                                <ul class="navbar-nav block">
+                                                <li class="nav-item">
+                                                    <a class="nav-link" href="index.php">Inicio</a>
+                                                </li>
+                                                <li class="nav-item">
+                                                    <a class="nav-link" href="listado.php">Productos</a>
+                                                </li>
+                                                <li class="nav-item">
+                                                    <a class="nav-link" href="usuario.php">Perfil</a>
+                                                </li>
+                                                <li class="nav-item">
+                                                    <a class="nav-link" href="faq.php">FAQ</a>
+                                                </li>
+                                                <li class="nav-item">
+                                                    <a class="nav-link" href="contacto.php">Contacto</a>
+                                                </li>
+                                                <li class="nav-item">
+                                                    <a class="nav-link" href="registro.php"> <ion-icon name="person"></ion-icon>Registrarse</a>
+                                                </li>
+                                                <li class="nav-item">
+                                                    <a class="nav-link" href="login.php"> <ion-icon name="exit"></ion-icon> Iniciar sesion</a>  
+                                                </li>
+                                                <li class="nav-item">
+                                                    <a class="nav-link" href="listado.php"><ion-icon name="cart"></ion-icon>Carrito</a>
+                                                </li>
+                                                </ul>
+                                            </div>   
+                                </nav>
+                    </header>
+                    </div>
+                    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+                    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+                    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    </body>
+</html>

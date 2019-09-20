e60218600ac3b0743133363e140040c3c15c202d
MatiasMart <matiasmartinelli12@gmail.com>
2019-09-20 12:58:57 -0300

    si siguen sin andar los css voy a romper todo

---
+++ footer.php
@@ -0,0 +1,13 @@
+<footer>
+    <div class="logotipo" >
+        <img src="img/logopsport.jpg" alt="logo de la empresa">
+    </div>
+
+    <div class="info" >
+
+    </div>
+
+    <div class="contacto" >
+
+    </div>
+ </footer>

37ec268c3e2ae21afadf8c4d3718ed09e7d04c1e
MatiasMart <matiasmartinelli12@gmail.com>
2019-11-01 19:33:17 -0300

    agregamos clases

---
+++ usuario/class-adiministrador.php
@@ -0,0 +1,19 @@
+<?php 
+    require_once("class-usuario.php");
+
+
+    class Administrador extends Usuario {
+
+
+        public function agregarProducto(){
+            // la dejamos vacia para completarla cuando sepamos usar mysql
+        }
+
+        public function removerProducto(){
+            // la dejamos vacia para completarla cuando sepamos usar mysql
+        }
+
+
+    }
+
+?>

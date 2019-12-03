5db3f79e741fa6069852fd8121f2dca24edd77ed
santibriozzo <santibriozzo@gmail.com>
2019-12-02 21:42:09 -0300

    auth en header

--- blog/resources/views/layout.blade.php
+++ blog/resources/views/layout.blade.php
@@ -9,70 +9,93 @@
     <script src="https://unpkg.com/ionicons@4.5.10-0/dist/ionicons.js"></script>
     <link rel="stylesheet" href="//use.fontawesome.com/releases/v5.0.7/css/all.css">
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
     @yield("links")
     <title>@yield('titulo')</title>
 </head>
 <body>
     <header class="row col-sm-12 col-md-12 col-lg-12 margencero" style="background-color:#0E0034;">
-                   <div class="col-sm-12 col-md-12 col-lg-12 navbar navbar-expand-md navbar-dark">
-                                <div class="logo col-sm-12 col-md-6 col-lg-3 justify-content-center">
-                                    <a class="navbar-brand" href="/"><img class="col-sm-12 col-md-6 col-lg-12" src="\storage\logoppal.png" alt="logo de la empresa"></a>
-                                
-                                </div>
-                                <nav class="col-sm-12 col-md-6 col-lg-4 buscar navbar navbar-light">
-                                        <form class="form-inline flex-wrap ">
-                                            <input class="caja_busc" type="search" placeholder="¿Qué estas buscando?" aria-label="Search">
-                                            <button type="submit"><ion-icon name="search" class="icono"></ion-icon></button>
-                                        </form>
-                                </nav>
-                            </div>
-                            <nav class=" col-sm-12 col-md-12 col-lg-12 navbar navbar-expand-md navbar-dark">   
-                                            <div class="hamburguesa align-items-right">
-                                                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
-                                                <span class="navbar-toggler-icon"></span>
-                                                </button>   
-                                            </div>
-                                            <div class="collapse navbar-collapse" id="collapsibleNavbar">                                   
-                                                <ul class="navbar-nav block">
-                                                    <li class="nav-item">
-                                                        <a class="nav-link color_menu" href="../recursos/index.php">Inicio</a>
-                                                    </li>
-                                                    <li class="nav-item">
-                                                        <a class="nav-link" href="../producto/listado.php">Productos</a>
-                                                    </li>
-                                                    <li class="nav-item">
-                                                        <a class="nav-link" href="../recursos/faq.php">FAQ</a>
-                                                    </li>
-                                                    <li class="nav-item">
-                                                        <a class="nav-link" href="../usuario/contacto.php">Contacto</a>
-                                                    </li>
-                                                    <li class="nav-item">
-                                                        <a class="nav-link" href="../usuario/registro.php"> <ion-icon name="person"></ion-icon>Registrarse</a>
-                                                    </li>
-                                                    <li class="nav-item">
-                                                        <a class="nav-link" href="../usuario/login.php"> <ion-icon name="exit"></ion-icon> Iniciar sesion</a>  
-                                                    </li>
-                                                </ul>      
-
-                            <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-
-
-
-
+        <div class="col-sm-12 col-md-12 col-lg-12 navbar navbar-expand-md navbar-dark">
+                    <div class="logo col-sm-12 col-md-6 col-lg-3 justify-content-center">
+                        <a class="navbar-brand" href="/"><img class="col-sm-12 col-md-6 col-lg-12" src="\storage\logoppal.png" alt="logo de la empresa"></a>
+                    
+                    </div>
+                    <nav class="col-sm-12 col-md-6 col-lg-4 buscar navbar navbar-light">
+                            <form class="form-inline flex-wrap ">
+                                <input class="caja_busc" type="search" placeholder="¿Qué estas buscando?" aria-label="Search">
+                                <button type="submit"><ion-icon name="search" class="icono"></ion-icon></button>
+                            </form>
+                    </nav>
+        </div>
+        <nav class=" col-sm-12 col-md-10 col-lg-10 navbar navbar-expand-md navbar-dark">   
+            <div class="hamburguesa align-items-right">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+                <span class="navbar-toggler-icon"></span>
+                </button>   
+            </div>
+            <div class="collapse navbar-collapse" id="collapsibleNavbar">                                   
+                <ul class="navbar-nav block">
+                    <li class="nav-item">
+                        <a class="nav-link color_menu" href="../recursos/index.php">Inicio</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="../producto/listado.php">Productos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="../recursos/faq.php">FAQ</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="../usuario/contacto.php">Contacto</a>
+                    </li>
+                    <li class="nav-item">
+                        @if (Route::has('login'))
+                            @auth     
+                                @guest
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                    </li>
+                                    @if (Route::has('register'))
+                                        <li class="nav-item">
+                                                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                        </li>
+                                    @endif
+                                        @else
+                                            <ul class="navbar-nav block"> 
+                                                <li class="nav-item dropdown">
+                                                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                                        <img src="/storage/{{ Auth::user()->img}}" alt="" height="40">
+                                                        {{ Auth::user()->nombre }} <span class="caret"></span>
+                                                    </a>
+                                                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                                        <a class="dropdown-item" href="{{ route('logout') }}"
+                                                            onclick="event.preventDefault();
+                                                                            document.getElementById('logout-form').submit();">
+                                                            {{ __('Logout') }}
+                                                        </a>
+                                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                                            @csrf
+                                                        </form>
+                                                    </div>
+                                                </li>
+                                            </ul>
+                                @endguest                                
+                                @else
+                                    <ul class="navbar-nav block">
+                                        <li class="nav-item">
+                                            <a href="{{ route('login') }}" class="nav-link">Iniciar Sesion</a>
+                                        </li>
+                                        @if (Route::has('register'))
+                                        <li class="nav-item">
+                                            <a href="{{ route('register') }}" class="nav-link">Registrarse</a>
+                                        </li>
+                                        @endif
+                                    </ul>   
+                            @endauth
+                        @endif     
+                    </li>
+                </ul>
+            </div>        
+        </nav>                                    
     </header>
 
     @yield('principal')
@@ -206,6 +229,7 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script src="{{ asset('js/app.js') }}" defer></script>
 </body>
 
 </html>

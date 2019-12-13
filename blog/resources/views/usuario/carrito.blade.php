bd87ad0343b0ecd52b3150cb94350943fec67c0a
santibriozzo <santibriozzo@gmail.com>
2019-12-13 19:41:40 -0300

    Carrito de compras con foreach

--- blog/resources/views/usuario/carrito.blade.php
+++ blog/resources/views/usuario/carrito.blade.php
@@ -14,7 +14,7 @@
 
 @section('principal')
     <div class="container">
-   <div class="card shopping-cart">
+         <div class="card shopping-cart">
             <div class="card-header bg-dark text-light">
                 <i class="fa fa-shopping-cart" aria-hidden="true"></i>
                 Carrito
@@ -23,42 +23,44 @@
             </div>
             <div class="card-body">
                     <!-- PRODUCT -->
-                    <div class="row">
-                        <div class="col-12 col-sm-12 col-md-2 text-center">
-                                <img class="img-responsive" src="http://placehold.it/120x80" alt="prewiew" width="120" height="80">
-                        </div>
-                        <div class="col-12 text-sm-center col-sm-12 text-md-left col-md-4">
-                            <h4 class="product-name"><strong>Nombre Producto</strong></h4>
-                            <h4>
-                                <small>Descripcion Producto</small>
-                            </h4>
-                        </div>
-                        <div class="col-12 col-sm-12 text-sm-center col-md-6 text-md-right row">
-                            <div class="col-3 col-sm-3 col-md-6 text-md-right pre" style="padding: 0px;padding-top: 10px;">
-                                <h6><strong>$2000</strong></h6>
+                    @foreach ($losProductos as $producto)
+                        <div class="row">
+                            <div class="col-12 col-sm-12 col-md-2 text-center">
+                                    <img class="img-responsive" src="http://placehold.it/120x80" alt="prewiew" width="120" height="80">
                             </div>
-                            <div class="col-4 col-sm-4 col-md-4">
-                                <div class="quantity">
-                                    <input type="button" value="+" class="plus">
-                                    <input type="number" step="1" max="99" min="1" value="1" title="Qty" class="qty"
-                                           size="4">
-                                    <input type="button" value="-" class="minus">
+                            <div class="col-12 text-sm-center col-sm-12 text-md-left col-md-4">
+                                <h4 class="product-name"><strong>{{$producto->'titulo'}}</strong></h4>
+                                <h4>
+                                    <small>{{$producto->'descripcion'}}</small>
+                                </h4>
+                            </div>
+                            <div class="col-12 col-sm-12 text-sm-center col-md-6 text-md-right row">
+                                <div class="col-3 col-sm-3 col-md-6 text-md-right pre" style="padding: 0px;padding-top: 10px;">
+                                    <h6><strong>{{$producto->'precio'}}</strong></h6>
+                                </div>
+                                <div class="col-4 col-sm-4 col-md-4">
+                                    <div class="quantity">
+                                        <input type="button" value="+" class="plus">
+                                        <input type="number" step="1" max="99" min="1" value="1" title="Qty" class="qty"
+                                            size="4">
+                                        <input type="button" value="-" class="minus">
+                                    </div>
+                                </div>
+                                <div class="col-2 col-sm-2 col-md-2 text-right">
+                                    <button type="button" class="btn btn-outline-danger btn-xs">
+                                        <i class="fa fa-trash" aria-hidden="true"></i>
+                                    </button>
                                 </div>
                             </div>
-                            <div class="col-2 col-sm-2 col-md-2 text-right">
-                                <button type="button" class="btn btn-outline-danger btn-xs">
-                                    <i class="fa fa-trash" aria-hidden="true"></i>
-                                </button>
-                            </div>
                         </div>
+                        <hr>
+                    <div class="pull-right">
+                        <a href="" class="btn btn-outline-secondary pull-right">
+                            Actualizar carrito
+                        </a>
                     </div>
-                    <hr>
-                <div class="pull-right">
-                    <a href="" class="btn btn-outline-secondary pull-right">
-                        Actualizar carrito
-                    </a>
                 </div>
-            </div>
+            @endforeach 
             <div class="card-footer">
                 <div class="coupon col-md-5 col-sm-5 no-padding-left pull-left">
                     <div class="row">
@@ -73,7 +75,7 @@
                 <div class="pull-right" style="margin: 10px">
                     <a href="" class="btn btn-success pull-right">Checkout</a>
                     <div class="pull-right" style="margin: 5px">
-                        Total: <b>$5000</b>
+                        Total: <b>{{$producto->'precio'}}</b>
                     </div>
                 </div>
             </div>
